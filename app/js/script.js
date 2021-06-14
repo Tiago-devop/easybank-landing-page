b116cefbe6653eb5eb22aa538dad9caeb610acf4
Tiago-devop <tiagodev@protonmail.com>
2021-06-14 17:24:34 -0300

    styling animated mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,25 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElements = document.querySelectorAll(".has-fade");
 
-btnHamburger.addEventListener("click", function () {
-  console.log("click hamburger");
-
+btnHamburger.addEventListener("click", () => {
   if (header.classList.contains("open")) {
     // Close Hamburger Menu
+    body.classList.remove("no-scroll");
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    fadeElements.forEach((element) => {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
   } else {
     // Open Hamburger Menu
+    body.classList.add("no-scroll");
     header.classList.add("open");
-    overlay.classList.remove("fade-out");
-    overlay.classList.add("fade-in");
+    fadeElements.forEach((element) => {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
   }
 });
